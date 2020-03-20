1a18ab79011e05d86c265b927ecd58bafda34eaa
ADiks <antondikson@gmail.com>
2020-03-20 17:42:04 +0200

    three day

--- script.js
+++ script.js
@@ -33,19 +33,30 @@ const showCity =  (input, list) => {
         const filterCity = city.filter((item) => {
 
             //возврат списка город
-           
-            const lowerItem = item.name.toLowerCase();
-            return lowerItem.includes(input.value.toLowerCase());
+			const lowerItem = item.name.toLowerCase();
+			
+            return lowerItem.startsWith(input.value.toLowerCase());
             
             
         });
-        
-        //вывод город в список по введеным данным
+		
+		
+		//вывод город в список по введеным данным
+		filterCity.sort((a, b) => {
+			if(a.name > b.name){
+				return 1;
+			}
+			if(a.name < b.name) {
+				return -1;
+			}
+			return 0;
+		});		
         filterCity.forEach((item) => {
 
-            //создание елемента списка
+			//создание елемента списка
+			
             const li = document.createElement('li');
-            li.classList.add('dropdown__city');
+			li.classList.add('dropdown__city');
             li.textContent = item.name;
             list.append(li);
         });
@@ -84,6 +95,36 @@ const getData = (url, callback) => {
     request.send();
 };
 
+
+const renderCheapDay = (cheapTicket) => {
+
+	console.log(cheapTicket);
+};
+
+const renderCheapAll = (cheapTickets) => {
+	cheapTickets.sort(function (a, b) {
+		if (a.value > b.value) {
+		  return 1;
+		}
+		if (a.value < b.value) {
+		  return -1;
+		}
+		// a должно быть равным b
+		return 0;
+	  });
+	console.log(cheapTickets);
+};
+
+const renderCheap = (data, date) => {
+	const CheapTicketAll = JSON.parse(data).best_prices;
+	const CheapTicketDay = CheapTicketAll.filter((item) => item.depart_date === date);
+	renderCheapAll(CheapTicketAll);
+	renderCheapDay(CheapTicketDay);
+	
+	
+}; 
+
+
 //ввод данных в форму "Откуда" и передача данных функции вывода
 inputCitiesFrom.addEventListener('input' , () => {
     showCity(inputCitiesFrom, dropdownCitiesFrom);
@@ -104,23 +145,33 @@ dropdownCitiesTo.addEventListener('click', (event) => {
     selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
 
+formSearch.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = {
+        from: city.find((item) => inputCitiesFrom.value === item.name).code,
+        to: city.find((item) => inputCitiesTo.value === item.name).code,
+        when: inputDateDepart.value,
+    }
+
+	const requestData = `?depart_date=${formData.when}&origin=${formData.from}&destination=${formData.to}&one_way=true`;
+	
+
+	getData(proxy + calendar + requestData, (response) => {
+		renderCheap(response, formData.when);		
+	});
+});
 
 //получение городов
-/*
 getData(proxy + citiesAPI, (data) =>{  
     city = JSON.parse(data).filter((item) => {
         return item.name;
-    });
+	});
     
 });
-*/
+
 
 
 //получения рейса 
-getData(calendar + '?origin=KGD&destination=SVX&depart_date=2020-05-25&one_way=false', (data) => {
-   console.log(JSON.parse(data));
-  
-});
- 
+
 
 
